43cc0f1a955cf56b6d15a1ecc36726de7add3359
NEDOWL <101130390+NEDOWL@users.noreply.github.com>
2025-04-18 11:15:05 +0000

    d

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/form', methods=['POST'])
 def home():
-    return render_template("index.html")
+    name = request.form.get('name')
+    return f'Hello, {name}!'
 
 if __name__ == "__main__":
     app.run(app.run(host='194.87.95.213', port=8080), debug=True)
